b58d98d6bfbd6e41cac553c2ad58955189dc9c92
KIMJINGUN <thehappypool@naver.com>
2021-07-11 02:05:12 +0900

    am2

---
+++ SpringOperation.java
@@ -0,0 +1,12 @@
+
+public class SpringOperation {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World".length()); 
+		System.out.println("Hello, leezche ... leezche ... leezche ... bye. ".replace("leezche", "egoing"));
+		System.out.println("Hello, [[[name]]] ... leezche ... leezche ... bye. ".replace("[[[name]]]", "coding"));
+
+
+	}
+
+}
---
+++ StringApp.java
@@ -0,0 +1,16 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); // string
+		System.out.println('H'); // character - 작은 따옴표는 한 문자를 나타냄
+		System.out.println("H"); // 가능
+		
+		System.out.println("Hello "
+				+ "World");
+		
+		System.out.println("Hello \nWorld"); // \n new line
+		System.out.println("Hello \"World\""); // \문자 기존의 문법에서 탈출시켜 일반 문자처럼 쓸수있음 escape
+		
+	}
+}
